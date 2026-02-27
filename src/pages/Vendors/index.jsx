b4acc4987eca1167cff0563c352b746a2842ca9f
shafi-afridi <shafistacks@gmail.com>
2026-02-28 02:06:55 +0500

    Page title updated

--- src/pages/Vendors/index.jsx
+++ src/pages/Vendors/index.jsx
@@ -34,7 +34,7 @@ const Vendors = () => {
 
       <div className="max-w-[1600px] mx-auto px-4 sm:px-6 py-6 sm:py-8">
         <h1 className="text-[32px] font-bold text-[#3D464F] mb-6">
-          Project Recommendations
+          Procurement
         </h1>
 
         <VenAISearchBanner />
